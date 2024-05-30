4fc25f418105ed07c85b9472e889ee03bb374d4a
Anshul Ramautar Yadav <96102972+2124mca1134@users.noreply.github.com>
2024-05-30 12:01:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TodoList from "./Tdlinput"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <TodoList heading="Enter your items for list......!"/>
+    </div>
+  );
+}
+
+export default App;
